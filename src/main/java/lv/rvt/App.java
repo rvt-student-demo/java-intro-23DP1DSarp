524f24475cf48f8814e382128ef84b326223d271
23DP1DSarp <166118195+23DP1DSarp@users.noreply.github.com>
2024-09-03 11:03:58 +0000

    saglabāts kā atgadinājums nākamai stundai

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -7,7 +7,11 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println("Hello world!");
+        // Once Upon a Time
+
+        System.out.println("Once upon a time");
+        System.out.println("there was");
+        System.out.println("a program");
     }
 
 }
