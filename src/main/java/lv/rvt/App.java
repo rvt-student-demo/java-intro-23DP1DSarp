2686a28e03b64c43ac5ed8722107e0cccfad2718
23DP1DSarp <166118195+23DP1DSarp@users.noreply.github.com>
2024-09-10 10:31:40 +0000

    Uzdevums 0

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -2,16 +2,29 @@ package lv.rvt;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.util.Scanner;
 
 public class App 
 {
     public static void main( String[] args )
     {
-        // Once Upon a Time
+        // Izveidojam skaneri vienu reizi un pielietojam visā programmā
+        
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Once upon a time");
-        System.out.println("there was");
-        System.out.println("a program");
+        System.out.println("Ievadiet savu vārdu: ");
+
+        String firstName = scanner.nextLine();
+
+        System.out.println("Ievadiet savu uzvārdu: ");
+
+        String lastName = scanner.nextLine();
+
+        System.out.println("Ievadiet savu grupu: ");
+
+        String group = scanner.nextLine();
+
+        System.out.println("Sveiki, " + firstName + " " + lastName + " " + group + ". Prieks jūs redzēt!");
     }
 
 }
