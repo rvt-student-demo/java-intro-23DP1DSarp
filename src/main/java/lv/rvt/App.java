87359396b5b4566afdcb3a217bb8fbe7afd0aa72
23DP1DSarp <166118195+23DP1DSarp@users.noreply.github.com>
2024-09-17 11:06:53 +0000

    Grades and Points

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -13,24 +13,44 @@ public class App
         Scanner scanner = new Scanner(System.in);
         
 
-        int number1 = Integer.valueOf(scanner.nextLine());
-        int number2 = Integer.valueOf(scanner.nextLine());
+        int points = Integer.valueOf(scanner.nextLine());
+        
         
 
         // Salīdzinām divus ciparus
 
-        if (number1 > number2) {
+        if (points < 0) {
 
-            System.out.println("Greater number is: " + number1);
+            System.out.println("impossible!");
 
 
-        } else if (number1 < number2) {
+        } else if (points >= 0 && points <= 49) {
 
-            System.out.println("Greater number is: " + number2);
+            System.out.println("failed");
+
+        } else if (points >= 50 && points <= 59) {
+
+            System.out.println("1");
+
+        } else if (points >= 60 && points <= 69) {
+
+            System.out.println("2");
+
+        }  else if (points >= 70 && points <= 79) {
+
+            System.out.println("3");
+
+        }   else if (points >= 80 && points <= 89) {
+
+            System.out.println("4");
+
+        }   else if (points >= 90 && points <= 100) {
+
+            System.out.println("5");
 
         } else {
 
-            System.out.println("The numbers are equal!");
+            System.out.println("incredible!");
 
         }
         
