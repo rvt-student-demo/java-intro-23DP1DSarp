ec4c2636311f71795e1e167db5968dc97961685f
23DP1DSarp <166118195+23DP1DSarp@users.noreply.github.com>
2024-09-24 10:51:30 +0000

    Sum of a sequence (fix)

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -11,13 +11,15 @@ public class App
         // Izveidojam skaneri vienu reizi un pielietojam visā programmā
         
         Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Last number?");
         
         Integer number = Integer.valueOf(scanner.nextLine());
 
         int numberSum = 0;
 
         for (int i = 0; i <= number; i++) {
-            System.out.println(i);
+            
             numberSum += i;
         }  
 
