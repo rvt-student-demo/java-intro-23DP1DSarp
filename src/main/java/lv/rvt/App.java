f4c4df50021faa84dc1e18a1fd99d9333ef6f45a
23DP1DSarp <166118195+23DP1DSarp@users.noreply.github.com>
2024-10-08 06:51:36 +0000

    Star sign

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -10,23 +10,97 @@ public class App
     {
 
 
-        Scanner scanner = new Scanner(System.in);
+        printStars(5);
+        printStars(3);
+        printStars(9);
 
-    
+        System.out.println("");
 
-        division(2, 10);
+        printSquare(4);
+
+        System.out.println("");
+
+        printRectangle(17, 3);
+
+        System.out.println("");
+
+        printTriangle(4);
+
+        
         
        }
 
-       public static void division(int intFirst, int intSecond) {
-        for (int i=intFirst; i < intSecond + 1; i++){
-            if (i % 3 == 0) {
-                System.out.println(i);
+       public static void printStars(int intNumber) {
+
+            int startNumber = 1;
+
+            while(startNumber <= intNumber){
+                System.out.print("*");
+                startNumber++;
             }
+            System.out.println("");
         }
-       }
+
+        public static void printSquare(int intSize) {
+
+            int startLineNumber = 1;
+            int startRowNumber = 1;
+
+            while(startRowNumber <= intSize){
+                while(startLineNumber <= intSize){
+                    System.out.print("*");
+                    startLineNumber++;
+                }
+               System.out.println("");
+               startRowNumber++;
+               startLineNumber = 1;
+            }
+
+        }
+
+        public static void printRectangle(int width, int height) {
+
+            int startLineNumber = 1;
+            int startRowNumber = 1;
+
+            while(startRowNumber <= height){
+                while(startLineNumber <= width){
+                    System.out.print("*");
+                    startLineNumber++;
+                }
+               System.out.println("");
+               startRowNumber++;
+               startLineNumber = 1;
+            }    
+            
+        }
+
+        public static void printTriangle(int size) {
+
+            int startLineNumber = 1;
+            int startRowNumber = 1;
+
+            
+                while(startRowNumber <= size){
+                    while(startLineNumber <= startRowNumber){
+                    System.out.print("*");
+                    startLineNumber++;
+                    }
+
+                System.out.println("");
+                startLineNumber = 1;
+                startRowNumber++;
+
+                }
+              
+            
+        }
+
+
+        
+}
             
 
-}
+
 
 
