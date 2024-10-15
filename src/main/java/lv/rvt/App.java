9f0fc17792e712150886f93d854792d752532fa5
23DP1DSarp <166118195+23DP1DSarp@users.noreply.github.com>
2024-10-15 07:48:42 +0000

    Third element

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -3,104 +3,29 @@ package lv.rvt;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.util.Scanner;
+import java.util.*;
 
 public class App 
 {
     public static void main( String[] args )
     {
 
+        Scanner scanner = new Scanner(System.in);
 
-        printTriangle(4);
+        ArrayList<String> wordList = new ArrayList<>();
 
-        System.out.println("");
+        String name = scanner.nextLine();
 
-        christmasTree(4);
-        christmasTree(10);
-        
+
+        while (name != "") {
+            name = scanner.nextLine();
+            wordList.add(name);
+        }
+       
+        System.out.println(wordList.get(2));
         
        }
 
        
 
-        public static void printTriangle(int size) {
-
-            int startLineNumber = 1;
-            int startRowNumber = 1;
-            int starCount = 1;
-            int counter = 1;
-            
-
-            
-               
-                    while(startRowNumber <= size){
-                    
-            
-                    while(startLineNumber <= size+2){
-                    System.out.print(" ");
-                    size--;
-                    startLineNumber++;
-                    }
-                    while (starCount <= counter) {
-                        System.out.print("*");
-                        starCount++;
-                    }
-
-                    counter++;
-                    starCount = 1;
-                    size = 4;
-                    System.out.println("");
-                    startRowNumber++;
-
-                }
-            
-            
-        }
-
-        public static void christmasTree(int height) {
-
-            int spacesNumber = 1;
-            int counter = 1;
-            
-            for(int i = 0; i < height; i++ ){
-                printSpaces(spacesNumber, height);
-
-                printStars(spacesNumber, height, counter);
-
-                System.out.println("");
-                counter = counter + 2;
-                spacesNumber--;
-            }
-            
-        
-            for (int i = 0; i < 2; i++) {
-                for (int a = 0; a <= height - 2; a++) {
-                System.out.print(" ");
-                
-                }
-                
-                System.out.println("***");
-            
-            }
-            
-            
-            
-        }
-
-            public static void printSpaces(int spacesNumber, int height){
-                for (int i = 0; i <= spacesNumber + height - 2; i++) {
-                    System.out.print(" ");
-                }
-            }
-
-            public static void printStars(int spacesNumber, int height, int counter){
-                for (int i = 0; i <= counter - 1; i++) {
-                    System.out.print("*");
-                }
-
-        
-
-        }
-
-    }
-
-
+}
