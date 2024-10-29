ddfff5d30cfbc2f9a4b86bf4a618c649f35bf11b
23DP1DSarp <166118195+23DP1DSarp@users.noreply.github.com>
2024-10-29 12:10:17 +0000

    Product (saglabāšana)

---
+++ src/main/java/lv/rvt/Product.java
@@ -0,0 +1,13 @@
+package lv.rvt;
+
+public class Product {
+
+
+    private String name;
+    private int price;
+
+        public static void main(String[] args) {
+            this.price = 1.1;
+        }
+
+}
