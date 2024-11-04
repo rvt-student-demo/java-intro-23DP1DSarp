a17f3bf90699530f18c8587deba10d204abaec28
23DP1DSarp <166118195+23DP1DSarp@users.noreply.github.com>
2024-11-04 19:53:34 +0000

    Product

--- src/main/java/lv/rvt/Product.java
+++ src/main/java/lv/rvt/Product.java
@@ -1,13 +1,31 @@
 package lv.rvt;
 
 public class Product {
-
-
+    
     private String name;
-    private int price;
+    private double price;
+    private int quantity;
 
-        public static void main(String[] args) {
-            this.price = 1.1;
-        }
+
+    public Product(String initialName, double initialPrice, int initialQuantity) {
+        this.name = initialName;
+        this.price = initialPrice;
+        this.quantity = initialQuantity;
+
+        printProduct();
+        
+        
+    }
+
+
+    public static void main(String[] args) {
+        Product myObj = new Product("Banana", 1.1, 13);
+    }
+
+    public void printProduct() {
+        System.out.println(this.name + ", price " + this.price + ", " + this.quantity + " pcs" );
+    }
+
+
 
 }
