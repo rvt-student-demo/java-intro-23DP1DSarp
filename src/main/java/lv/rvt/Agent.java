f286acd6d62bbe9814541e2090097efdefd48574
23DP1DSarp <166118195+23DP1DSarp@users.noreply.github.com>
2024-11-05 11:41:47 +0000

    Agent

---
+++ src/main/java/lv/rvt/Agent.java
@@ -0,0 +1,20 @@
+package lv.rvt;
+
+public class Agent {
+
+   private String agent1;
+   private String agent2;
+
+
+   public Agent(String agent1, String agent2){
+    this.agent1 = agent1;
+    this.agent2 = agent2;
+   }
+
+   public String toString(){
+        return "My name is Bond, " + this.agent1 + " " + this.agent2;
+   }
+
+    
+    
+}
