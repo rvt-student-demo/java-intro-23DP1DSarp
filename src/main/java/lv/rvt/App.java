4de9a4b6b1568ae7b189aeaedf3ffee8d8fb4acd
23DP1DSarp <166118195+23DP1DSarp@users.noreply.github.com>
2024-11-12 11:56:20 +0000

    Uzdevums no tafeles

--- src/main/java/lv/rvt/App.java
+++ src/main/java/lv/rvt/App.java
@@ -2,20 +2,58 @@ package lv.rvt;
 
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.nio.file.Paths;
 import java.util.*;
 
 public class App 
 {
-    public static void main(String[] args) throws Exception{
-        BufferedReader reader = Utils.getReader("data.csv");
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader reader = Utils.getReader("persons.csv");
+    
+        ArrayList<Person> persons = new ArrayList<>();
+     
         String line;
+       
+        try (Scanner scanner = new Scanner(Paths.get("persons.csv"))) {
 
-
-        while ((line = reader.readLine()) != null) {
-            System.out.println(line);
+            while (scanner.hasNextLine()) {
+                line = scanner.nextLine();
+        
+                String[] parts = line.split(",");
+                String name = parts[0];
+                int age = Integer.valueOf(parts[1]);
+        
+                persons.add(new Person(name, age));
+            }
         }
-    }
 
-}    
+        for (Person name: persons) {
+            System.out.println(name);
+        }
+
+        String[] parts = {"name", "age", "weight", "height"};
+     
+        Person pers1 = new Person(parts[0],parts[1],parts[2],parts[3]);
+        persons.add(pers1);
+     
+        
+     
+
+     
+        reader.close();
+       }
+    
+    
+    
+}
+
+
+        // Neprintēt kolonnu virsrakstus
+        // Output: Id: <Id>; username; <usrnm>; passsword; <pwd>
+        // println() print()
+
+
+
 
 
--- src/main/java/lv/rvt/Person.java
+++ src/main/java/lv/rvt/Person.java
@@ -1,41 +1,81 @@
 package lv.rvt;
 
+import java.nio.file.Paths;
+import java.util.Scanner;
+
 public class Person {
 
     private String name;
     private int age;
+    private int weight;
+    private int height;
+
+    public Person(String name) {
+        this.name = name;
+        this.age = 0;
+        this.weight = 0;
+        this.height = 0;
+    }
+
+    public Person(String name2, int age2) {
+        //TODO Auto-generated constructor stub
+    }
+
+    public Person(String string, String string2, String string3, String string4) {
+        //TODO Auto-generated constructor stub
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public void growOlder() {
+        this.age = this.age + 1;
+    }
+
+    public void setHeight(int newHeight) {
+        this.height = newHeight;
+    }
+
+    public void setWeight(int newWeight) {
+        this.weight = newWeight;
+    }
+
+    public double bodyMassIndex() {
+        double heightDivByHundred = this.height / 100.0;
+        return this.weight / (heightDivByHundred * heightDivByHundred);
+    }
+
+    
+
+    @Override
+    public String toString() {
+        return this.name + ", age " + this.age + " years";
+    }
 
     public static void main(String[] args) {
-        Person ada = new Person("Ada");
-        Person antti = new Person("Antti");
-        Person martin = new Person("Martin");
-
-        ada.printPerson();
-        antti.printPerson();
-        martin.printPerson();
-    }
-
-    public Person(String initialName) {
-        this.age = 0;
-        this.name = initialName;
-    }
-
-    public void printPerson() {
-        System.out.println(this.name + ", age " + this.age + " years");
+        Person person = new Person("Dan");
+        System.out.println(person);
     }
 
     // Getter method
-
+    /* 
     public String getGroup() {
         return this.
         group;
     }
-
+    
+    */
 
     // Setter method
 
+    /* 
     public String setGroup() {
         this.group = group;
     }
-
+    */
 }
